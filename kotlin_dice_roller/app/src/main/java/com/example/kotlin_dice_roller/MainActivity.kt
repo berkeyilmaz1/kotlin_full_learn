4ceeccbe241b67e959e09374fe161da6f692d711
berkeyilmaz1 <berkeyilmaz189@gmail.com>
2024-06-28 15:43:08 +0300

    first commit

---
+++ kotlin_dice_roller/app/src/main/java/com/example/kotlin_dice_roller/MainActivity.kt
@@ -0,0 +1,88 @@
+package com.example.kotlin_dice_roller
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.kotlin_dice_roller.ui.theme.Kotlin_dice_rollerTheme
+import androidx.compose.runtime.getValue
+
+import androidx.compose.runtime.setValue
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Kotlin_dice_rollerTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                )
+
+                {
+                    DiceRollerApp()
+                }
+            }
+        }
+    }
+
+
+    @Composable
+    fun DiceWithButtonAndImage(modifier: Modifier = Modifier ) {
+        var result by remember { mutableStateOf(1) }
+
+        val imageResource = when (result) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        Column(modifier = Modifier.fillMaxSize().wrapContentSize(Alignment.Center), horizontalAlignment = Alignment.CenterHorizontally) {
+
+            Image(
+                painter = painterResource(id = imageResource),
+                contentDescription = result.toString()
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(onClick = { result = (1..6).random() }) {
+                Text(text = stringResource(id = R.string.Roll))
+            }
+        }
+    }
+
+
+    @Preview(showBackground = true)
+    @Composable
+    fun DiceRollerApp() {
+        DiceWithButtonAndImage(
+            modifier = Modifier
+                .fillMaxSize()
+                .wrapContentSize(Alignment.Center)
+        )
+    }
+}
